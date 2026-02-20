baf42ea230f790d2dc5cccc8763f84c3a0d60159
SeleiXi <bzhanthresh@gmail.com>
2026-02-20 12:48:15 +0800

    fix: CI test not pass

--- src/test/java/com/starfinder/StarFinderApplicationTests.java
+++ src/test/java/com/starfinder/StarFinderApplicationTests.java
@@ -1,8 +1,10 @@
+package com.starfinder;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class StarFinderApplicationTests {
 
     @Test
     void contextLoads() {
